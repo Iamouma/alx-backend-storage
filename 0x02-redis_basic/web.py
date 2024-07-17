4bad5cda143f34cf47c13b8659c6ca230750a850
Iamouma <stephenouma452@gmail.com>
2024-07-17 19:08:54 +0000

    Redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,54 @@
 #!/usr/bin/env python3
 """
-web cache and tracker
+Module to provide a cache decorator for web page retrieval.
 """
+
 import requests
 import redis
-from functools import wraps
+from typing import Callable
 
-store = redis.Redis()
+redis_client = redis.Redis()
 
 
-def count_url_access(method):
-    """ Decorator counting how many times
-    a URL is accessed """
-    @wraps(method)
-    def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
+def cache_decorator(func: Callable) -> Callable:
+    """
+    Decorator to cache the result of a function
+    with a specified expiration time.
+    """
+    def wrapper(*args, **kwargs):
+        """
+        Generate a cache key based on the function name and arguments
+        """
+        cache_key = f"cache:{func.__name__}:{args}"
 
-        count_key = "count:" + url
-        html = method(url)
+        cached_result = redis_client.get(cache_key)
+        if cached_result:
+            return cached_result.decode('utf-8')
 
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
+        result = func(*args, **kwargs)
+
+        redis_client.setex(cache_key, 10, result)
+
+        return result
+
     return wrapper
 
 
-@count_url_access
+@cache_decorator
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+    """
+    Get the HTML content of a particular URL
+    and cache the result with an expiration time of 10 seconds.
+    """
+    redis_client.incr(f"count:{url}")
+
+    cached_data = redis_client.get(url)
+    if cached_data:
+        return cached_data.decode('utf-8')
+
+    response = requests.get(url)
+    html_content = response.text
+
+    redis_client.setex(url, 10, html_content)
+
+    return html_content
