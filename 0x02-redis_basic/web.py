3ebfa2889d16bff668f97dd0d4e7a895da9a7d6c
Iamouma <stephenouma452@gmail.com>
2024-07-17 09:39:37 +0000

    Push

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,40 +1,18 @@
-
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
+# -*- coding: utf-8 -*-
+""" create a cache system with redis """
 import redis
 import requests
-from functools import wraps
-from typing import Callable
+
+r = redis.Redis()
 
 
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
-
-
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
-    @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
-
-
-@data_cacher
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    return requests.get(url).text
-
+    """ get page """
+    count  = 0
+    
+    r.set(f"count:{url}", count)
+    resp = requests.get(url)
+    r.incr(f"count:{url}")
+    r.setex(f"cached: {url}", 10, r.get(f"cached: {url}"))
+    return resp.text
